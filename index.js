2442c476690cc47cddbb9edc962316dc1136a100
Shalahuddin360 <shalahuddinislam792@gmail.com>
2023-05-03 22:58:47 +0600

    git ignore file are added

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors')
 const port =process.env.PORT || 5000;
 
 const chef =require('./data/chefs.json');
+
+const recipes =require('./data/recipes.json');
 app.use(cors())
 app.get('/',(req,res)=>{
   res.send('FoodHub is comming soon')  
@@ -12,6 +14,26 @@ app.get('/',(req,res)=>{
 app.get('/chef',(req,res)=>{
   res.send(chef)
 })
+app.get('/recipes',(req,res)=>{
+  res.send(recipes)
+})
+app.get('/recipes/:id',(req,res)=>{
+  const id = req.params.id;
+  const selectedRecipe = recipes.find(r=> r.recipe_id=== id);
+  res.send(selectedRecipe)
+})
+app.get('/chef/:id' ,(req,res)=>{
+  const id = parseInt(req.params.id);
+  if(id===0){
+    res.send(recipes)
+  }
+  else{
+    const chefRecipes = recipes.filter(r=> parseInt(r.chef_id) ===id);
+    res.send(chefRecipes)
+  }
+
+})
+
 app.listen(port , ()=>{
     console.log(`FoodHub API is running on port : ${port}`)
 })
