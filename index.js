e77684e22766df78ca97c458a81be63a95e52436
Shalahuddin360 <shalahuddinislam792@gmail.com>
2023-05-04 16:59:57 +0600

    corse file are added in the project

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express =require('express');
-const app =express();
+const express = require('express');
+const app = express();
 const cors = require('cors')
-const port =process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
-const chef =require('./data/chefs.json');
+const chef = require('./data/chefs.json');
 
-const recipes =require('./data/recipes.json');
+const recipes = require('./data/recipes.json');
 app.use(cors())
-app.get('/',(req,res)=>{
-  res.send('FoodHub is comming soon')  
+app.get('/', (req, res) => {
+  res.send('FoodHub is comming soon')
 })
 
-app.get('/chef',(req,res)=>{
+app.get('/chef', (req, res) => {
   res.send(chef)
 })
-app.get('/recipes',(req,res)=>{
+app.get('/recipes', (req, res) => {
   res.send(recipes)
 })
-app.get('/recipes/:id',(req,res)=>{
+app.get('/recipes/:id', (req, res) => {
   const id = req.params.id;
-  const selectedRecipe = recipes.find(r=> r.recipe_id=== id);
+  const selectedRecipe = recipes.find(r => r.recipe_id === id);
   res.send(selectedRecipe)
 })
-app.get('/chef/:id' ,(req,res)=>{
+app.get('/chef/:id', (req, res) => {
   const id = parseInt(req.params.id);
-  if(id===0){
+   if (id === 0) {
     res.send(recipes)
   }
-  else{
-    const chefRecipes = recipes.filter(r=> parseInt(r.chef_id) ===id);
+  else {
+    const chefRecipes = recipes.filter(r => parseInt(r.chef_id) === id);
     res.send(chefRecipes)
   }
 
-})  
+})
 
-app.listen(port , ()=>{
-    console.log(`FoodHub API is running on port : ${port}`)
+app.listen(port, () => {
+  console.log(`FoodHub API is running on port : ${port}`)
 })
